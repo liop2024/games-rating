2352dc21afa7beb22e4248109639fa8b52ec777a
liop2024 <167613561+liop2024@users.noreply.github.com>
2024-04-27 10:39:48 +0300

    Update config.js

--- appModules/api/config.js
+++ appModules/api/config.js
@@ -5,3 +5,11 @@ const endpoints = {
 
 module.exports = endpoints;
 
+const PATH_TO_RATING_FILE = "./dataset/rating.json";
+const WEIGHT = {
+  gameplay: 2,
+  design: 1,
+  idea: 3,
+};
+module.exports = { PATH_TO_RATING_FILE, WEIGHT };
+
