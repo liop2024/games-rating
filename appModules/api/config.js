9fd9286a86bf2cbcfb7d1f8896b4a1019df9d002
liop2024 <167613561+liop2024@users.noreply.github.com>
2024-04-28 10:22:02 +0300

    Update config.js

--- appModules/api/config.js
+++ appModules/api/config.js
@@ -1,15 +1,5 @@
-const BASE_URL = 'https://api-code-2.practicum-team.ru';
-const endpoints = {
-    games: `${BASE_URL}/games`,
+const BASE_URL = "https://api-code-2.practicum-team.ru";
+const endpoins = {
+games: ${BASE_URL}/games,
 };
-
-module.exports = endpoints;
-
-const PATH_TO_RATING_FILE = "./dataset/rating.json";
-const WEIGHT = {
-  gameplay: 2,
-  design: 1,
-  idea: 3,
-};
-module.exports = { PATH_TO_RATING_FILE, WEIGHT };
-
+module.exports = endpoins;
